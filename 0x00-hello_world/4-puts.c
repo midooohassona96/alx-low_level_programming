8a1505fd10338ffb728eaced812f01ac3e32437b
midooohassona96 <midooohassan96@gmail.com>
2022-09-08 17:37:09 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
-#!/bin/bash
 #include<stdio.h>
 /**
  * main - put the string in the put function
- *
  * Description: using the main function
  * This programs print "Programming is like building a multilingual puzzle
- * Return: 0
+ * Return:0
  */ 
 int main(void)
 {
