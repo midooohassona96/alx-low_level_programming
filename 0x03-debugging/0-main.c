32dfd94114c3bd6e58cdb4bfaa125ad88cb3d3bf
midooohassona96 <midooohassan96@gmail.com>
2022-09-15 16:50:25 -0700

    1. Like, comment, subscribe

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 int main(void)
 
@@ -6,9 +6,19 @@ int main(void)
 
 int i;
 
+printf("Infinite loop incoming :(\n");
+
 i = 0;
 
-positive_or_negative(i);
+while (i < 10)
+
+{
+
+putchar(i);
+
+}
+
+printf("Infinite loop avoided! \\o/\n");
 
 return (0);
 
