a54f2983efee96c9d99d04dda340652d59d40b8f
midooohassona96 <midooohassan96@gmail.com>
2022-09-15 10:50:59 -0700

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,57 +4,33 @@ int main(void)
 
 {
 
-int num = 1;
+int num;
 
-while (num++ < 100)
-
-{
-
-if ((num % 3 == 0) && (num % 5 == 0))
+for (num = 1; num <= 100; num++)
 
 {
 
-printf("FizzBuzz ");
+if ((num % 3) == 0 && (num % 5) == 0)
 
-}
+printf("FizzBuzz");
 
 else if ((num % 3) == 0)
 
-{
-
-printf("Fizz ");
-
-}
+printf("Fizz");
 
 else if ((num % 5) == 0)
 
-{
-
-if (num != 100)
-
-{
-
-printf("Buzz ");
-
-}
-
-else
-
-{
-
 printf("Buzz");
 
-}
-
-}
-
 else
 
-{
+printf("%d", num);
 
-printf("%d ", num);
+if (num == 100)
 
-}
+continue;
+
+printf(" ");
 
 }
 
