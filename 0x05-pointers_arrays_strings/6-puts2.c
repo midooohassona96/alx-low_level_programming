ab7f6336633458fc45fd382a100dee29e73a5320
midooohassona96 <midooohassan96@gmail.com>
2022-09-19 17:40:43 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include "main.h"
+
+void puts2(char *str)
+
+{
+
+int i = 0;
+
+while (*(str + i) != '\0')
+
+{
+
+if (i % 2 == 0)
+
+putchar(*(str + i));
+
+i++;
+
+}
+
+putchar(10);
+
+}
