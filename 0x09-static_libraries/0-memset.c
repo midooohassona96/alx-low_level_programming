18944b10a7ec910b87cf219f35a9aa267902abcf
midooohassona96 <midooohassan96@gmail.com>
2022-09-30 08:40:07 -0700

    1. Without libraries what have we? We have no past and no future

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+char *_memset(char *s, char b, unsigned int n)
+
+{
+
+while (n)
+
+{
+
+s[n - 1] = b;
+
+n--;
+
+}
+
+return (s);
+
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+#include <stdio.h>
+
+int _abs(int c)
+
+{
+
+if (c > 0 || c == 0)
+
+{
+
+return (c);
+
+}
+
+else
+
+return (c * -1);
+
+}
